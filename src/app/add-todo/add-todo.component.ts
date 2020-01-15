33eba849430dc99a953af7325bb91b3c585a7d7a
doctor-glitch <harinrajan2014@gmail.com>
2020-01-15 14:45:15 +0530

    formvalidations implemented

--- src/app/add-todo/add-todo.component.ts
+++ src/app/add-todo/add-todo.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { HelloService } from '../services/hello.service';
 import { TodoService } from '../services/todo.service';
 import { Router } from '@angular/router';
@@ -11,30 +11,48 @@ import { Router } from '@angular/router';
 })
 export class AddTodoComponent implements OnInit {
   todoForm: any;
-
+  error = true;
   constructor(private fb: FormBuilder, private ser: HelloService, private todoService: TodoService, private router: Router) {
     this.todoForm = fb.group({
-      name: '',
-      description: ''
+      name: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      description: new FormControl('', [
+        Validators.required,
+        Validators.minLength(5),
+      ])
     });
   }
 
   add() {
-    if (!this.todoForm.value.name || !this.todoForm.value.description) {
-      alert('invalid');
+    if (!this.todoForm.valid) {
+      // if (this.todoForm.get('name').errors != null) {
+      //   alert('name error');
+      //   console.log(this.todoForm.get('name'));
+      // }
+      // if (this.todoForm.get('description').errors != null) {
+      //   alert('description error');
+      // }
     } else {
       this.todoService.add(this.todoForm.value.name, this.todoForm.value.description).subscribe(data => {
         this.todoForm.reset();
         this.router.navigate(['todo']);
       });
     }
+    return;
   }
 
-  delete(){
-
-  }
+  // delete() { }
 
   ngOnInit() {
   }
 
+  get(controlName) {
+    return this.todoForm.get(controlName);
+  }
+
+  hasError(controlName){
+    return this.get(controlName).errors && (this.get(controlName).dirty || this.get(controlName).touched);
+  }
 }
